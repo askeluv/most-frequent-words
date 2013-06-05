ac4f8c0df3dfa6b62e272805e06b65d182c0d2e8
Alexander Svanevik <alexander.svanevik@gmail.com>
2013-06-05 23:51:53 +0200

    Fixed relative URLs

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
 from bs4 import BeautifulSoup as bs
 import urllib2
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 application = Flask(__name__)
 URL = { 'Spanish' : 'http://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/Spanish1000',
         'Italian' : 'http://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/Italian1000',
         'French' : 'http://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/French_wordlist_opensubtitles_5000'}
 
-def get_content_from_web(language):
-    'Gets words from a en.wiktionary.org URL for most frequent words'
-    url = URL[language]
+def from_web(language):
+    'Gets the 100 most frequent words from a wiktionary.org page'
     opener = urllib2.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-    soup = bs(opener.open(url).read()) # open, read page, and make BS object
+    soup = bs(opener.open(URL[language]).read()) # read page, make BS object
     rows = soup.findAll('tr') # finds all <tr> elements
     
-    # grabs the second column: an <a> element
-    # skips the header, and only gets 100 rows
+    # skip header, get 100 rows, and the <a> element
     words = [x.findAll('td')[1].a for x in rows[1:101]]
     
     # make the relative paths absolute, and add language #
@@ -30,8 +28,7 @@ def index():
 
 @application.route('/lang/<lang>')
 def lang(lang):
-    return render_template('language.html', language='%s' % title(lang),\
-			    words=get_content_from_web(language))
+    return render_template('lang.html', lang=lang.title(),words=from_web(lang))
 
 if __name__ == '__main__':
 	application.run(debug=True)
