2409de5e9c1b95466dda2deaca16f022a763ec98
Ana Viz <ana.vizcarrondo@gmail.com>
2013-11-16 00:21:41 +0100

    setting angular

---
+++ static/wordsController.js
@@ -0,0 +1,10 @@
+
+function wordsController($scope) {
+    $scope.lang = "español";
+
+    $scope.words = [
+        {text:'de', rank:1},
+        {text:'para', rank:2},
+        {text:'casa', rank:3},
+        {text:'roca', rank:4}];
+}
