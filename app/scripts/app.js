43b45bfa7e230ae21944a9ddc782bcb42d7503ac
Ana Viz <ana.vizcarrondo@gmail.com>
2014-01-25 15:27:56 +0100

    - Navigation: key arrow press event handling - Change href to ng-href

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,5 +9,5 @@ mfwApp.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.when('/word/:rank', {templateUrl: 'views/mfw-list.html', controller: 'mfwListCtrl'});
         $routeProvider.when('/word/:rank/example/:id', {templateUrl: 'views/mfw-example.html', controller: 'mfwListCtrl'});
-        $routeProvider.otherwise({redirectTo: '/'});
+        $routeProvider.otherwise({redirectTo: '/word/1'});
     }]);
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,7 +1,14 @@
 'use strict';
 
+var ARROW_LEFT = 37;
+var ARROW_UP = 38;
+var ARROW_RIGHT = 39;
+var ARROW_DOWN = 40;
+var NUMBER_OF_WORDS = 4;
+var NUMBER_OF_EXAMPLES = 3;
+
 angular.module('controllers', []).
-    controller('mfwListCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
+    controller('mfwListCtrl', ['$scope', '$routeParams', '$location', function ($scope, $routeParams, $location) {
         $scope.lang = "español";
 
         $scope.words = [{word:'de', rank:1, examples:['Soy de Madrid','De nada','De aqui para alla']},
@@ -12,6 +19,64 @@ angular.module('controllers', []).
         var wRank = $routeParams.rank;
         $scope.exampleId = +$routeParams.id;
         $scope.w = $scope.words[wRank-1];
-        $scope.wordsLength = $scope.words.length;
-        $scope.examplesLength = $scope.w ? $scope.w.examples.length : null;
+        $scope.wordsLength = NUMBER_OF_WORDS;
+        $scope.examplesLength = NUMBER_OF_EXAMPLES;
+
+        //////////////////////////////////////////////
+        ///////////////// FUNCTIONS //////////////////
+        //////////////////////////////////////////////
+
+        $scope.keyUp = function(keyEvent) {
+            var wordRank = +$routeParams.rank;
+            var exampleId = $routeParams.id ? +$routeParams.id : -1;
+
+            switch(keyEvent.keyCode)
+            {
+                case ARROW_LEFT:
+                    if(exampleId !== -1){
+                        $scope.navigateLeft(wordRank, exampleId);
+                    }
+                    break;
+                case ARROW_UP:
+                    if(wordRank > 1 && exampleId === -1) {
+                        $scope.navigateUp(wordRank);
+                    }
+                    break;
+                case ARROW_RIGHT:
+                    if(exampleId < NUMBER_OF_EXAMPLES-1){
+                        $scope.navigateRight(wordRank, exampleId);
+                    }
+                    break;
+                case ARROW_DOWN:
+                    if(wordRank < NUMBER_OF_WORDS && exampleId === -1) {
+                        $scope.navigateDown(wordRank);
+                    }
+                    break;
+            }
+        };
+
+        $scope.navigateLeft = function(wordRank, exampleId) {
+            if(exampleId === 0){
+                $scope.go('/word/'+ wordRank);
+            }else{
+                $scope.go('/word/'+ wordRank +'/example/'+ (exampleId - 1));
+            }
+        };
+
+        $scope.navigateUp = function(wordRank) {
+            $scope.go('/word/'+ (wordRank - 1));
+        };
+
+        $scope.navigateRight = function(wordRank, exampleId) {
+            $scope.go('/word/'+ wordRank +'/example/'+ (exampleId + 1));
+        };
+
+        $scope.navigateDown = function(wordRank) {
+            $scope.go('/word/'+ (wordRank + 1));
+        };
+
+        $scope.go = function (path) {
+            $location.path(path);
+        };
+
     }]);
