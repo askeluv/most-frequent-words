53fcd8abdce7508efc0022c7e56f4ab6c8c24eef
Ana Viz <ana.vizcarrondo@gmail.com>
2014-05-03 13:43:22 +0200

    loads of stuff

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -11,16 +11,23 @@ angular.module('controllers', []).
     controller('mfwListCtrl', ['$scope', '$routeParams', '$location', function ($scope, $routeParams, $location) {
         $scope.lang = "español";
 
-        $scope.words = [{word:'de', rank:1, examples:['Soy de Madrid','De nada','De aqui para alla']},
-            {word:'para', rank:2, examples:['Para ya','Para que quieres eso?','Para para aaguaaas']},
-            {word:'casa', rank:3, examples:['Telefono, mi casa','El chico se casa mañana','Casa casa .. casablanca!']},
-            {word:'roca', rank:4, examples:['Hay rocas en el mar','Mi amigo Roca','Roca roca, .. roca and roll!']}];
+        $scope.words = [
+	        {word:'de', rank:1, percent: 10, examples:['Soy de Madrid','De nada','De aqui para alla']},
+            {word:'para', rank:2, percent: 13, examples:['Para ya','Para que quieres eso?','Para para aaguaaas']},
+            {word:'casa', rank:3, percent: 15, examples:['Telefono, mi casa','El chico se casa mañana','Casa casa .. casablanca!']},
+            {word:'roca', rank:4, percent: 16, examples:['Hay rocas en el mar','Mi amigo Roca','Roca roca, .. roca and roll!']}
+        ];
 
-        var wRank = $routeParams.rank;
-        $scope.exampleId = +$routeParams.id;
-        $scope.w = $scope.words[wRank-1];
+		if($routeParams){
+			var wRank = $routeParams.rank;
+			$scope.exampleId = +$routeParams.id;
+			$scope.w = $scope.words[wRank-1];
+		}
+
         $scope.wordsLength = NUMBER_OF_WORDS;
         $scope.examplesLength = NUMBER_OF_EXAMPLES;
+
+
 
         //////////////////////////////////////////////
         ///////////////// FUNCTIONS //////////////////
