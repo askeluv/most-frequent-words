6ec636aaf864c99b5edafe5d8157bb3db9ae0e15
Alexander Svanevik <alexander.svanevik@gmail.com>
2014-05-25 17:37:37 +0200

    Added highlighter in filters.js

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -1,10 +1,24 @@
 'use strict';
 
-/* Filters */
-
-angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+/**
+ * Wraps the
+ * @param text {string} haystack to search through
+ * @param search {string} needle to search for
+ * @param [caseSensitive] {boolean} optional boolean to use case-sensitive searching
+ */
+angular.module('filters', [])
+    .filter('highlight', function() {
+        return function(text, search, caseSensitive) {
+            if (text && (search || angular.isNumber(search))) {
+                text = text.toString();
+                search = search.toString();
+                if (caseSensitive) {
+                    return text.split(search).join('<span class="highlighted">' + search + '</span>');
+                } else {
+                    return text.replace(new RegExp(search, 'gi'), '<span class="highlighted">$&</span>');
+                }
+            } else {
+                return text;
+            }
+        };
+    });
