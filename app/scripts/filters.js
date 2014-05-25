86828f3377a363141dab1b29455d9057f71cf8b8
Ana Viz <ana.vizcarrondo@gmail.com>
2014-05-25 18:55:57 +0200

    regex: match only whole words

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -8,15 +8,11 @@
  */
 angular.module('filters', [])
     .filter('highlight', function() {
-        return function(text, search, caseSensitive) {
+        return function(text, search) {
             if (text && (search || angular.isNumber(search))) {
                 text = text.toString();
                 search = search.toString();
-                if (caseSensitive) {
-                    return text.split(search).join('<span class="highlighted">' + search + '</span>');
-                } else {
-                    return text.replace(new RegExp(search, 'gi'), '<span class="highlighted">$&</span>');
-                }
+                return text.replace(new RegExp("\\b"+search+"\\b", 'gi'), '<span class="highlighted">$&</span>');
             } else {
                 return text;
             }
